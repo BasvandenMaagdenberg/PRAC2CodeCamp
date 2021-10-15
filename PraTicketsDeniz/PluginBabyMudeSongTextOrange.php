572ac71851585d352f7afe19030b663b03c68c8e
deniz hrnjic <73642343+denizzzzzzz@users.noreply.github.com>
2021-10-15 12:40:21 +0200

    php

---
+++ PraTicketsDeniz/PluginBabyMudeSongTextOrange.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>song-text-Plug-Muse</title>
+</head>
+<body>
+<h1>Plug In Baby</h1>
+<img src="img/MusePlugInBaby.jpg">
+<h2>by Muse</h2>
+<p>
+I've exposed your lies, baby 
+The underneath no big surprise 
+Now it's time for changing 
+And cleansing everything 
+To forget your love 
+</p>
+<p>
+My plug in baby 
+Crucifies my enemies 
+When I'm tired of giving 
+My plug in baby 
+In unbroken virgin realities 
+Is tired of living 
+</p>
+<p>
+Don't confuse 
+Baby you're gonna lose 
+Your own game 
+Change me 
+And replace the envying 
+To forget your love 
+</p>
+<p>
+My plug in baby 
+Crucifies my enemies 
+When I'm tired of giving 
+My plug in baby 
+In unbroken virgin realities 
+And tired of living 
+And I've seen your loving 
+Mine is gone 
+</p>
+</body>
+</html>
