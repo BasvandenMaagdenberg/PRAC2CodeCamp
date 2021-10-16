b7f7f5562859efd387e9fb4eb235b4818fbb8dee
stefano <72141994+D261324@users.noreply.github.com>
2021-10-16 14:22:58 +0200

    update header met navigatie setup en styling
    styling voor de nav is gedaan door middel van een voorbeeld van w3schools
    de structuur is gemaakt door mij en gaat door linken naar een pagina met links naar de bijbehorende pagina's

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <!doctype html>
 <html encoding="utf-8" lang="nl">
 
-<head>
-  <meta charset="utf-8">
-  <title>codecamp</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
-</head>
+<?php require_once'head.php'; ?>
 
 <body>
-
+<?php require_once'header.php';?>
   <!-- Add your site or application content here -->
   <p>Hello world! This is HTML5 Boilerplate.</p>
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
