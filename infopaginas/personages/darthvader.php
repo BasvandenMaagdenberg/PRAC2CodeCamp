2f5d485dde44f87f9dcd7eeeea0feff88577eb3a
basvandenmaagdenberg <71122658+BasvandenMaagdenberg@users.noreply.github.com>
2021-10-17 23:03:04 +0200

    head toevoegen

--- infopaginas/personages/darthvader.php
+++ infopaginas/personages/darthvader.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="../../css/main.css">
-    </head>
+<html encoding="utf-8" lang="nl">
+<?php require_once 'head.php'; ?>
     <body>
+
         <main>
+            <?php require_once '../../header.php';?>
             <div class="container"> 
                 <div class="darthvaderPage">
                     <h1>Darth Vader</h1>
