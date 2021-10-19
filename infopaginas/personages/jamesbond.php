0fc4dae823bd6078c5d3a2e938147d2f91d1ab6f
basvandenmaagdenberg <71122658+BasvandenMaagdenberg@users.noreply.github.com>
2021-10-19 11:51:24 +0200

    Roger Moore

---
+++ infopaginas/personages/jamesbond.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html encoding="utf-8" lang="nl">
+<?php require_once 'head.php'; ?>
+    <body>
+        <header>
+        <?php require_once '../../header.php';?>
+        </header>
+        <main>
+            <div class="InfoPaginaRogerMoore">
+                <div class="container"> 
+                    <div class="RogerMoorePage">
+                        <h1>Roger Moore</h1>
+                        <div class="InfoImg">
+                            <img src="../../img/roger-moore-james.png" alt="Img Roger Moore">
+                        </div>
+                        <div class="RogerMooretext">
+                            <p><b>Sir Roger George Moore (Stockwell, Lambeth, Londen, 14 oktober 1927 – Crans-Montana, Zwitserland, 23 mei 2017) was een Britse acteur die vooral bekend werd door zijn rollen in de series Ivanhoe, The Saint en The Persuaders (in Nederland uitgezonden als De Versierders en in België als De Speelvogels). Verder speelde hij de rol van geheim agent James Bond in zeven Bondfilms, die uitkwamen tussen 1973 en 1985.</b></p>
+
+                            <h2>James Bond</h2>
+                            <p>8	Live and Let Die	1973	Roger Moore	$ 7.000.000	$ 126.000.000	6,8[15]
+                                9	The Man with the Golden Gun	1974	$ 13.000.000	$ 97.600.000	6,8[16]
+                                10	The Spy Who Loved Me	1977	Lewis Gilbert	$ 14.000.000	$ 185.400.000	7,1[17]
+                                11	Moonraker	1979	$ 34.000.000	$ 210.300.000	6,3[18]
+                                12	For Your Eyes Only	1981	John Glen	$ 28.000.000	$ 194.900.000	6,8[19]
+                                13	Octopussy	1983	$ 27.500.000	$ 187.500.000	6,6[20]
+                                14	A View to a Kill	1985	$ 30.000.000	$ 152.400.000	6,3[21]
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
