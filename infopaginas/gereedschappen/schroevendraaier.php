e0f492c01179d8bdff202c0a95d6f5d560467e1d
FerryDeVries <71122367+FerryDeVries@users.noreply.github.com>
2021-10-21 09:57:53 +0200

    Added info page schroevendraaier

---
+++ infopaginas/gereedschappen/schroevendraaier.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html encoding="utf-8" lang="nl">
+<?php require_once 'head.php'; ?>
+    <body>
+        <header>
+            <?php require_once 'header.php';?>
+        </header>
+        <main>
+            <div class="InfoPaginaSchroevendraaier">
+                <div class="container"> 
+                    <div class="InfoPage">
+                        <h1>Schroevendraaier</h1>
+                        <div class="InfoImg">
+                            <img src="../../img/Schroevendraaier.jpg" alt="Img Schroevendraaier">
+                        </div>
+                        <div class="InfoText">
+                            <p><b>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
+
+Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers met een platte punt, alsook met een ster- of kruiskop. Goede schroevendraaiers zijn vervaardigd van speciale staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'</b></p>
+
+                            <h2>Schroevendraaier</h2>
+                            <p>In plaats van schroevendraaiers worden ook wel losse schroefbits in een schroefboormachine gebruikt. Deze zeskantige stiftjes zijn er in velerlei vormen en lengten. Zo zijn er kruis- of ster-, platte voor sleuf, zeskantige maar ook vierkante vormen. Variërend in lengte van enkele centimeters tot meer dan 10 centimeter De bits worden in een – meestal magnetische – houder geplaatst die vervolgens in de schroef- dan wel boormachine wordt geklemd. Toch gebruikt men niet voor alle schroeven een schroefboormachine. Horlogemakers en instrumentmakers gebruiken voor het fijne werk speciale kleine schroevendraaiers.
+
+Doorgaans worden schroeven rechtsom vastgedraaid en linksom los.
+
+Veelgebruikte types schroevendraaiers:
+                            </p>
+                            <li>sleufkopschroevendraaier (plat);</li>
+                            <li>phillipsschroevendraaier PH (kruiskop zonder verdere markering);</li>
+                            <li>pozidrivschroevendraaier PZ (kruiskop met extra sterretje)</li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
