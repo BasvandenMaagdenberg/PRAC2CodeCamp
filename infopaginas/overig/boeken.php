23b146e84c16ce0c5429a03ac8bcc0a735f94052
FerryDeVries <71122367+FerryDeVries@users.noreply.github.com>
2021-10-21 12:02:12 +0200

    Added Infopage boeken

---
+++ infopaginas/overig/boeken.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html encoding="utf-8" lang="nl">
+<?php require_once 'head.php'; ?>
+    <body>
+        <header>
+            <?php require_once '../../header.php';?>
+        </header>
+        <main>
+            <div class="InfoPaginaBoeken">
+                <div class="container"> 
+                    <div class="InfoPage">
+                        <h1>Boeken</h1>
+                        <div class="InfoImg">
+                            <img src="../../img/Boeken.jpg" alt="Img Boeken">
+                        </div>
+                        <div class="InfoText">
+                            <p><b>Het boek is een veelgebruikte manier om informatie vast te leggen in de vorm van tekst en afbeeldingen en om deze informatie weer te geven, op te slaan en te verspreiden. Het is een document van vaak niet al te kleine omvang.</b></p>
+
+                            <h2>Boeken</h2>
+                            
+                            <p>Voor de huidige boekvorm met pagina's, de codex, werd uitgevonden, bestond geschreven informatieoverdracht al in de vorm van inbeiteling op steen, boekrollen van papyrus of perkament, kleitabletten, stukken bot, leren vellen en dergelijke. Korte notities die niet bewaard hoefden te blijven, werden meestal op potscherven geschreven. Dit kwam zo omdat in ieder huishouden schalen, bekers en voorraadvaten van keramiek gebruikt werden en er geregeld wat brak zodat er aan scherven geen gebrek was.</p>
+                            <p>De auteur schrijft een boek meestal met een tekstverwerker, waarna een elektronisch manuscript (hier dus niet in de letterlijke zin van een handgeschreven versie) wordt gestuurd naar de uitgever. Een papieren boek wordt gedrukt door een drukker en gebonden door een boekbinder. Een distributeur verzorgt meestal de logistiek van de verspreiding naar boekhandels en andere verkooppunten; daarnaast zijn er systemen voor de distributie van e-boeken. Bij distributie en verkoop zijn boekdata van belang om het individuele boek vindbaar te maken te midden van de miljoenen leverbare boektitels.
+                            Bij het produceren van boeken zijn behalve auteur, uitgever en drukker onder meer grafische vormgevers, illustratoren, boekbandontwerpers, fotografen, redacteuren en correctoren betrokken, al naargelang de uitgave vereist.
+                            Een zeer bekende 'markt' voor nieuwe boeken, waar uitgevers uit de hele wereld elkaars uitgaven kunnen bekijken en titels ter vertaling kunnen kopen, is de jaarlijkse Frankfurter Buchmesse.
+                            Delen van boekoplagen die niet of niet snel genoeg verkocht zijn, gaan veelal in de wederverkoop. Er zijn boekenmarkten en boekenwinkels speciaal voor tweedehandsboeken of de ramsjmarkt; de grootste en bekendste daarvan is De Slegte.
+                            Bij literaire uitgaven is vooral de eerste druk van een boek van belang bij de latere antiquarische verkoop. Vooral de geschriften van bekende schrijvers kunnen dan aanzienlijk meer gaan opbrengen.</p>
+
+                            <h2>Soort boeken</h2>
+
+                            <li><b>Brochure</b>
+                            een brochure is een boek van geringe omvang. Een brochure bestaat uit meerdere vellen gevouwen papier, die in de rug bijeengehouden worden door bijvoorbeeld nietjes. Het aantal pagina's is altijd een veelvoud van vier.</li> <br>
+                            <li><b>Pocketboek</b>
+                            goedkoper, maar minder duurzaam dan een genaaid of gebonden boek is het pocketboek, kortweg de pocket. De rug van een pocket is gelijmd; de kaft is een papieren omslag met een plastic laagje en de bladzijden zijn meestal van een mindere papierkwaliteit.</li> <br>
+                            <li><b>E-boek</b>
+                            een boek kan ook de vorm hebben van een digitale tekst, al of niet met afbeeldingen. Men spreekt dan wel van een e-boek, dat gedownload kan worden of op een gegevensdrager staat. Het is te gebruiken door vanaf een scherm te lezen of uit te printen. Ook zijn inmiddels speciale e-bookreaders ontwikkeld, speciale apparatuur waarmee e-books gelezen kunnen worden, vaak met een speciaal soort scherm, zogenaamd e-paper. E-books kunnen ook gelezen worden op handhelds als Palm, Pocket PC en smartphones.</li> <br>
+                            <li><b>Leerboeken</b>
+                            een leerboek of studieboek is een boek waar men iets uit kan leren. Leerboeken worden vaak gebruikt in lesmethoden op een basisschool of binnen het voortgezet onderwijs of op de universiteit. Educatieve uitgeverijen produceren leerboeken. Ook een informatief boek is een soort leerboek.</li> <br>
+                            <li><b>Dwarsliggers</b>
+                            een kleiner boekformaat, te vergelijken met de eerder genoemde kleine Bijbel met zijn kleine lettertype en dunne papier, is de dwarsligger.</li>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
