d947993cb89c16bbdfa93e0601dfb65d16e41b4d
FerryDeVries <71122367+FerryDeVries@users.noreply.github.com>
2021-10-21 11:32:49 +0200

    Added info page hamers

---
+++ infopaginas/gereedschappen/hamers.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html encoding="utf-8" lang="nl">
+<?php require_once 'head.php'; ?>
+    <body>
+        <header>
+            <?php require_once '../../header.php';?>
+        </header>
+        <main>
+            <div class="InfoPaginaHamers">
+                <div class="container"> 
+                    <div class="InfoPage">
+                        <h1>Hamers</h1>
+                        <div class="InfoImg">
+                            <img src="../../img/Hamers.jpg" alt="Img Hamer">
+                        </div>
+                        <div class="InfoText">
+                            <p><b>Een hamer is een klop- of slagwerktuig. Een hamer bestaat uit een steel, het handvat, met aan het eind de zogenaamde hamerkop.
+                            De vorm en grootte en het materiaal van de hamerkop en de lengte van de steel hangen sterk af van het type hamer. Bij hamers met een ijzeren hamerkop staat vaak het aantal grammen in het metaal geslagen. Voor speciale uitvoeringen, zoals voor percussie-instrumenten, wordt ook de term mallet (uit het Engels) gebruikt.</b></p>
+
+                            <h2>Hamers</h2>
+                        <li><b>Gewone hamer</b>
+                        een metalen hamerkop van 50-300 gram op een houten steel van ca. 30 cm lang die onder meer wordt gebruikt om spijkers (draadnagels) in houten voorwerpen te slaan.</li> <br>
+                        <li><b>Klauwhamer</b>
+                        zoals een gewone hamer, maar de achterkant van de hamer heeft een platte V-groef waarin spijkers geklemd kunnen worden. De bovenkant is afgerond zodat de geklemde spijker uit het object gerold kan worden.</li> <br>
+                        <li><b>Vuist of moker</b>
+                        een zware hamer met een vierkante hamerkop van 1000-1500 gram op een steel van 30 cm lang, voor zwaar werk. Met een rubberen opzetstuk wordt een vuisthamer gebruikt om tegels of straatstenen in het zand te leggen voor het maken van bestrating.</li> <br>
+                        <li><b>Tegelhamer</b>
+                        houten hamer met grote houten kop en een korte steel. Om tegels te leggen.</li> <br>
+                        <li><b>Straathamer</b>
+                        een hamer om te straten, met een kromme zijde om stenen door midden te slaan, en een rechte afgeplatte zijde om stenen aan te slaan (bestraten).</li> <br>
+                        <li><b>Voorhamer</b>
+                        een hamer om sloopwerkzaamheden uit te voeren. Een hamerkop van een paar kilo op een steel van een meter lang.</li> <br>
+                        <li><b>Rubberen hamer</b>
+                        een rubberen hamer wordt vooral gebruikt bij het leggen van tegels op grond. Een kleine hamer met een rubberen gewicht die kan worden gebruikt voor voorzichtig werk, zoals het aankloppen van kleine lijmverbindingen in de houtbewerking. Kampeerders gebruiken een rubber hamer om de tentpinnen in de grond te slaan. Door (para)medici wordt soms een kleine hamer gebruikt met een zacht gedeelte. Deze heeft meestal twee gekleurde uiteinden.</li> <br>
+                        <li><b>Houten hamer</b>
+                        een hamer met een houten hamerkop die kan worden gebruikt voor voorzichtig werk zoals het vooruithelpen van een guts of beitel in de houtbewerking. De hamerkop is gemaakt van een harde houtsoort, meestal beukenhout. De steel is gemaakt van een taaie houtsoort. Dit is nodig om de klappen goed op te kunnen vangen. De hamerkop kan rond (cilindervormig) zijn of een blokvorm hebben.</li> <br>
+                        <li><b>Smidshamer</b>
+                        een hamer met een gevormd gewicht die kan worden gebruikt om (heet) ijzer op een aambeeld vorm te geven.</li>
+                              
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
