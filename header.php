37365c737069d527f66574377bcf9738ad4f3285
stefano <72141994+D261324@users.noreply.github.com>
2021-10-21 17:31:15 +0200

    update header
    - variable base_url toegevoegd om makkelijker te navigeren. de url van iedere link begint nu van af de basis map in htdocs.
    - ALLE header linkjes staan goed. navigeren via de header werkt correct

--- header.php
+++ header.php
@@ -1,37 +1,37 @@
+<?php $base_url = 'http://localhost/PRA2CodeCamp/';?>
 <div class="navbar">
+  <button class="dropbtn-home"><a href="<?php echo $base_url;?>index.php">home</a></button>
 
-<div class="dropdown">
-    <button class="dropbtn"> Dranken
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Warmen Dranken</a>
-      <a href="#">Fris Dranken</a>
-    </div>
+<div class="dropdown">    
+    <div class="dropdown">  
+      <button class="dropbtn"> Dranken
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="<?php echo $base_url;?>dranken/warm.php">Warmen Dranken</a>
+        <a href="<?php echo $base_url;?>dranken/frisdrank.php">Fris Dranken</a>
+      </div>
   </div>
+    </div>
 
   <div class="dropdown">
     <button class="dropbtn"> Infopagina's 
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="#">Gereedschappen</a>
-        <div class="dropdown-content inlist">
-          <a href="#" class="dropout">zagen</a>
-        </div>
-      <a href="#">Persona's</a>
-      <a href="#">boeken / externe verwijzingen</a>
+      <a href="<?php echo $base_url;?>infopaginas/catagorien.php">Gereedschappen</a>
+      <a href="<?php echo $base_url;?>infopaginas/personages/catagorien.php">Persona's</a>
+      <!-- <a href="<?php echo $base_url;?>overig/catagorien.php">boeken / externe verwijzingen</a> <--></-->
     </div>
   </div>
 
   <div class="dropdown">
-    <button class="dropbtn"> Artiesten en Songtexten
+    <button class="dropbtn"><a href="<?php echo $base_url;?>infopaginas/personages/catagorien.php"></a> Artiesten en Songtexten
       <i class="fa fa-caret-down"></i>
     </button>
-    <div class="dropdown-content">
-      <a href="#">Iron maiden</a>
-      <a href="#">Muse</a>
-      <a href="#">Queen</a>
+      <div class="dropdown-content">
+      <a href="<?php echo $base_url;?>songtexten/muse/catagorien.php">Muse</a>
+      <a href="<?php echo $base_url;?>songtexten/queen/catagorien.php">Queen</a>
     </div>
   </div>
 </div>
