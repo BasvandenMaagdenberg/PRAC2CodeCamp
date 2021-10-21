440383c42597ffe9b8c38cf1a836c7a26854d361
stefano <72141994+D261324@users.noreply.github.com>
2021-10-21 17:25:55 +0200

    update head.php
    - variable base_url aangemaakt voor het gemak van navigeren. dit maakt het makkelijker om van af de root van de website te navigeren.

--- head.php
+++ head.php
@@ -1,9 +1,12 @@
+<?php $base_url = 'http://localhost/PRA2CodeCamp/';?>
 <head>
   <meta charset="utf-8">
   <title>codecamp</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="<?php echo $base_url;?>css/normalize.css">
+  <link rel="stylesheet" href="<?php echo $base_url;?>css/main.css">
+
+
 </head>
