29c5315108afa09e26fd7b1a24c84454eb1abb79
stefano <72141994+D261324@users.noreply.github.com>
2021-11-05 12:27:57 +0100

    update jamesbond.php
    link naar footer aangepast en header

--- infopaginas/personages/jamesbond.php
+++ infopaginas/personages/jamesbond.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html encoding="utf-8" lang="nl">
-<?php require_once 'head.php'; ?>
+<?php require_once '../../head.php'; ?>
     <body>
         <header>
         <?php require_once '../../header.php';?>
@@ -65,7 +65,6 @@
                 </div>
             </div>
         </main>
-        <footer>
-        </footer>
+        <?php require_once '../../footer.php';?>
     </body>
 </html>
