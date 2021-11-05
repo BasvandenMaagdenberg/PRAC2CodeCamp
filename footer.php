ad399a83c73bd32327a13dbe8e9b0966a26479c3
stefano <72141994+D261324@users.noreply.github.com>
2021-11-05 12:14:23 +0100

    created footer
    footer aangemaakt

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+	<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Pariatur non minus, eaque perferendis aliquam accusamus possimus labore maxime, doloribus corrupti adipisci voluptas totam, modi aliquid et. Illo corrupti qui, unde?</p>
+</footer>
