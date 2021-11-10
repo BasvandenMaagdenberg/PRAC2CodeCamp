3d6764dd2def83e3a401ad2c039d08f1e50b2619
deniz hrnjic <73642343+denizzzzzzz@users.noreply.github.com>
2021-11-10 15:38:54 +0100

    Uprising MYBABY
    pagina Uprising gemaakt MYBABY

---
+++ songtexten/mybaby/uprising.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once '../../head.php'; ?>
+<body>
+    <header>
+        <?php require_once '../../header.php';  ?>
+    </header>
+    <main class="UprisingMybaby">
+        <div class="container">
+            <div class="Songtext-container">
+                <h1>Uprising</h1>
+                <h3>MYBABY</h3>
+                <div class="InfoImg">
+                    <img src="../../img/UprisingMybaby.jpg">
+                </div>
+                <div class="Songtext UprisingMybaby">
+
+
+                    <p>Allow me in your tub
+                    Just allow me in your club
+                    Allow me to drink from your cup
+                    I'll be coming back for more</p>
+                    
+                    <p>Allow me in your dream
+                    Allow me in your scene
+                    Just allow me on your knees
+                    I'll be coming back for more</p>
+
+                    <p>Sisters, keep on rising
+                    Brothers, keep on rising
+                    This girl keeps on rising
+                    Keep on rising up</p>
+
+                    <p>All I Believe in your school
+                    I Believe in your rules
+                    I Believe in your truth
+                    I'll be coming back for more</p>
+
+                    <p>I Believe in your land
+                    I Believe in that man
+                    I Believe in your command
+                    I'll be coming back for more</p>
+
+                    <p>Sisters, Brothers, Lovers
+                    Keep on rising up</p>
+
+                    <p>Sisters, keep on rising
+                    Brothers, keep on rising
+                    This girl keeps on rising
+                    Keep on rising up</p>
+
+                    <p>Uprising, uprising
+                    Uprising, uprising</p>
+
+                    <p>Sisters, Brothers, Lovers
+                    This girl keep on rising up</p>
+
+                    <p>Sisters, keep on rising
+                    Brothers, keep on rising
+                    This girl keeps on rising
+                    Keep on rising up</p>
+                    
+                </div>
+
+            </div>
+
+        </div>
+        
+    </main>
+    <?php require_once '../../footer.php';?>
+</body>
+</html>
