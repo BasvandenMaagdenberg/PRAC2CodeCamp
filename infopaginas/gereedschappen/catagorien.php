23789b6833bee1769a7c10db29c3297de0376e53
stefano <72141994+D261324@users.noreply.github.com>
2021-11-10 15:54:45 +0100

    cato container div weg gehaald
    fixed de gekkegeid  met de cards

--- infopaginas/gereedschappen/catagorien.php
+++ infopaginas/gereedschappen/catagorien.php
@@ -7,7 +7,7 @@
     <?php require_once'../../header.php';?>
 
     <main>
-      <div class="container cato-container">
+      <div class="container">
          <div class="cato-keuze-temp">
           <h3>zagen</h3>
           <img src="" alt="">
