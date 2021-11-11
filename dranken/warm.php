2826f55ebb35522d3104fadfc991f98534274d59
basvandenmaagdenberg <71122658+BasvandenMaagdenberg@users.noreply.github.com>
2021-11-11 11:55:47 +0100

    zagen

--- dranken/warm.php
+++ dranken/warm.php
@@ -21,20 +21,23 @@
                         
                             <h2>Voorbeelden van warme dranken zijn:</h2>
 
-                            <p><b>Koffie</b>
+                            <li><b>Koffie</b>
+                            <p>
                                 Koffie is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en gebrande pitten van de koffieplant (Coffea spp.) 
                                 die vanwege hun vorm koffiebonen worden genoemd. Koffie bevat het stimulerende middel cafeïne. De meeste soorten in het geslacht Coffea komen van nature voor in tropisch Afrika en op de eilanden in de Indische Oceaan. 
                                 Ze vinden hun oorsprong in Ethiopië, Jemen en Soedan.
                             </p>
-
-                            <a href="<?php echo $base_url;?>infopaginas/personages/georgeclooney.php">George Clooney</a>
-                            
-                            <p><b>Latte</b>
+                            <p>
+                              <a href="<?php echo $base_url;?>infopaginas/personages/georgeclooney.php">George Clooney</a>
+                            </p>
+                            <li><b>Latte</b>
+                            <p>
                                 Een caffè latte of latte is een in veel Europese en Amerikaanse gelegenheden geserveerde koffiedrank, gemaakt met espresso en warme melk. 
                                 Vermoedelijk ligt de oorsprong in de Verenigde Staten, waar de drank uitgevonden zou zijn om de espresso uit de opkomende koffiemachines wat te verzachten. 
                             </p> 
 
                             <li><b>Thee</b>
+                            <p>
                                 Thee is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant Camellia sinensis. 
                                 Alle als thee verkochte producten die niet van deze plant afkomstig zijn (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee. 
                                 De naam thee komt waarschijnlijk uit het Chinese dialect Minnanyu.
